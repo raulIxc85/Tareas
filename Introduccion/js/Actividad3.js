370a0e2d8393e2bdfa107a38ab6e4c350f489b75
Raul Ixcot <raul.ixc@gmail.com>
2018-02-11 23:48:39 -0600

    Subida de archivos

---
+++ Introduccion/js/Actividad3.js
@@ -0,0 +1,36 @@
+var mes=10;
+switch (mes){
+    case 1:
+        document.write("Es invierno");
+        break;
+    case 2:
+        document.write("Es invierno");
+        break;
+    case 3:
+        document.write("Es primavera");
+        break;
+    case 5:
+        document.write("Es primavera");
+        break;
+    case 6:
+        document.write("Es verano");
+        break;
+    case 8:
+        document.write("Es verano");
+        break;
+    case 9:
+        document.write("Es otoño");
+        break;
+    case 11:
+        document.write("Es otoño");
+        break;
+    case 12:
+        document.write("Es invierno");
+        break;
+    default:
+        document.write("El mes que ingreso no tiene definido una estacion !!!");
+        break;
+}
+document.write("<br>");
+document.write("Carne: 08140091<br/>");
+document.write("Nombre: Raul Estuardo Ixcot Cardenas");
